9afc8d737cffd544fe1e1b892af425e58f0de133
Roman <rart05@yahoo.com>
2025-07-13 18:20:56 +0200

    feat! : Added annotation @NoAuth for request with no required token

--- data/src/main/java/com/example/data/network/api/AuthApi.kt
+++ data/src/main/java/com/example/data/network/api/AuthApi.kt
@@ -4,13 +4,17 @@ import com.example.data.network.dto.auth.LoginRequestDTO
 import com.example.data.network.dto.auth.LoginResponseDTO
 import com.example.data.network.dto.auth.RegisterRequestDTO
 import com.example.data.network.dto.auth.RegisterResponseDTO
+import com.example.data.network.util.NoAuth
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthApi {
+
+    @NoAuth
     @POST("/api/auth/signup")
     suspend fun register(@Body request: RegisterRequestDTO): RegisterResponseDTO
 
+    @NoAuth
     @POST("/api/auth/login")
     suspend fun login(@Body request: LoginRequestDTO): LoginResponseDTO
 }
--- data/src/main/java/com/example/data/network/interceptor/AuthInterceptor.kt
+++ data/src/main/java/com/example/data/network/interceptor/AuthInterceptor.kt
@@ -1,10 +1,12 @@
 package com.example.data.network.interceptor
 
+import com.example.data.network.util.NoAuth
 import com.example.data.storage.UserStorageService
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import retrofit2.Invocation
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(
@@ -12,13 +14,26 @@ class AuthInterceptor @Inject constructor(
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val user = runBlocking { userStorage.userFlow.first() }
+        val request = chain.request()
 
-        val request = chain.request().newBuilder()
-        user?.token?.let {
-            request.addHeader("Authorization", "Bearer $it")
+        val invocation = request.tag(Invocation::class.java)
+        val noAuthAnnotation = invocation?.method()?.getAnnotation(NoAuth::class.java)
+
+        if (noAuthAnnotation != null) {
+            // Skip authentication for requests annotated with @NoAuth
+            return chain.proceed(request)
         }
 
-        return chain.proceed(request.build())
+        val user = runBlocking { userStorage.userFlow.first() }
+
+        val newRequest = request.newBuilder()
+            .apply {
+                if (!user?.token.isNullOrBlank()) {
+                    addHeader("Authorization", "Bearer ${user.token}")
+                }
+            }
+            .build()
+
+        return chain.proceed(newRequest)
     }
 }
