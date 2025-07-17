334c49c56de500b90b97b8148d115efb92ed4a7c
Roman <rart05@yahoo.com>
2025-07-17 21:10:45 +0200

    feat: added adapter for image preview

--- app/src/main/java/com/example/ep2024/ui/utils/ImagePreviewAdapter.kt
+++ app/src/main/java/com/example/ep2024/ui/utils/ImagePreviewAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.ep2024.ui.utils
 
-class ImagePreviewAdapter {
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ep2024.databinding.ItemImagePreviewBinding
+
+class ImagePreviewAdapter : ListAdapter<Uri, ImagePreviewAdapter.ImageViewHolder>(ImageDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val binding = ItemImagePreviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ImageViewHolder(private val binding: ItemImagePreviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(uri: Uri) {
+            binding.ivPreview.setImageURI(uri)
+        }
+    }
+
+    class ImageDiffCallback : DiffUtil.ItemCallback<Uri>() {
+        override fun areItemsTheSame(oldItem: Uri, newItem: Uri): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Uri, newItem: Uri): Boolean {
+            return oldItem == newItem
+        }
+    }
 }
