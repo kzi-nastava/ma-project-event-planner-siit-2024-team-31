2263110f3ae1fb620b0d154d51d66293c97ed23d
Roman <rart05@yahoo.com>
2025-07-17 21:05:17 +0200

    refactor: changed RegisterRequesDTO, decided to user Params instead of DTO

--- data/src/main/java/com/example/data/network/dto/auth/RegisterRequestDTO.kt
+++ data/src/main/java/com/example/data/network/dto/auth/RegisterRequestDTO.kt
@@ -1,8 +1,15 @@
 package com.example.data.network.dto.auth
 
 class RegisterRequestDTO(
-    val username: String,
     val email: String,
-    val password: String
-) {
-}
+    val password: String,
+    val firstName: String,
+    val lastName: String,
+    val role: String,
+    val phoneNumber: String?,
+    val country: String?,
+    val city: String?,
+    val address: String?,
+    val zipCode: String?,
+    val description: String?
+)
