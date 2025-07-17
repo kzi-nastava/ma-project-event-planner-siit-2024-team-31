849e4b3f845f1fe40907a0471f4f6d568fcbf89e
Roman <rart05@yahoo.com>
2025-07-17 21:07:42 +0200

    feat! : changed! signature for register request, changed login method signature

--- data/src/main/java/com/example/data/network/api/AuthApi.kt
+++ data/src/main/java/com/example/data/network/api/AuthApi.kt
@@ -5,16 +5,35 @@ import com.example.data.network.dto.auth.LoginResponseDTO
 import com.example.data.network.dto.auth.RegisterRequestDTO
 import com.example.data.network.dto.auth.RegisterResponseDTO
 import com.example.data.network.util.NoAuth
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface AuthApi {
 
     @NoAuth
+    @Multipart
     @POST("/api/auth/signup")
-    suspend fun register(@Body request: RegisterRequestDTO): RegisterResponseDTO
+    suspend fun register(
+        @Part("email") email: RequestBody,
+        @Part("password") password: RequestBody,
+        @Part("role") role: RequestBody,
+        @Part("firstName") firstName: RequestBody,
+        @Part("lastName") lastName: RequestBody,
+        @Part("phoneNumber") phoneNumber: RequestBody,
+        @Part("country") country: RequestBody,
+        @Part("city") city: RequestBody,
+        @Part("address") address: RequestBody,
+        @Part("zipCode") zipCode: RequestBody,
+        @Part("description") description: RequestBody?,
+        @Part photos: List<MultipartBody.Part>
+    ): Response<RegisterResponseDTO>
 
     @NoAuth
     @POST("/api/auth/login")
-    suspend fun login(@Body request: LoginRequestDTO): LoginResponseDTO
+    suspend fun login(@Body request: LoginRequestDTO): Response<LoginResponseDTO>
 }
