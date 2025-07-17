4a3385c9be6b1683ad403fbe844285e03e8da72d
Roman <rart05@yahoo.com>
2025-07-17 21:08:16 +0200

    fix: added Context to provide method

--- data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -1,5 +1,6 @@
 package com.example.data.di
 
+import android.content.Context
 import com.example.data.network.api.AuthApi
 import com.example.data.repository.AuthRepositoryImpl
 import com.example.data.storage.UserStorageService
@@ -7,6 +8,7 @@ import com.example.domain.repository.AuthRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -16,8 +18,12 @@ object RepositoryModule {
 
     @Provides
     @Singleton
-    fun provideAuthRepository(api: AuthApi, userStorage: UserStorageService): AuthRepository {
-        return AuthRepositoryImpl(api, userStorage)
+    fun provideAuthRepository(
+        api: AuthApi,
+        userStorage: UserStorageService,
+        @ApplicationContext context: Context
+    ): AuthRepository {
+        return AuthRepositoryImpl(api, userStorage, context)
     }
 
 }
