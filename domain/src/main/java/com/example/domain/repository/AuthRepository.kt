65ec9b3961060cff3dbd14e2435109d19e385815
Roman <rart05@yahoo.com>
2025-07-17 17:34:15 +0200

    feat! : added nnew params for registration, updated UseCase and AuthRepo

--- domain/src/main/java/com/example/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/example/domain/repository/AuthRepository.kt
@@ -1,8 +1,9 @@
 package com.example.domain.repository
 
+import com.example.domain.entity.auth.RegisterParams
 import com.example.domain.util.Resource
 
 interface AuthRepository {
-    suspend fun register(username: String, email: String, password: String): Resource<Unit>
     suspend fun login(email: String, password: String): Resource<Unit>
+    suspend fun register(params: RegisterParams): Resource<Unit>
 }
--- domain/src/main/java/com/example/domain/usecase/auth/RegisterUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/auth/RegisterUseCase.kt
@@ -1,13 +1,33 @@
-package com.example.domain.usecase.auth;
+package com.example.domain.usecase.auth
 
+import com.example.domain.entity.auth.RegisterParams
+import com.example.domain.entity.user.Role
 import com.example.domain.repository.AuthRepository
 import com.example.domain.util.Resource
+import javax.inject.Inject
 
-class RegisterUseCase(private val repository: AuthRepository) {
-    suspend operator fun invoke(username: String, email: String, password: String): Resource<Unit> {
-        if (username.isBlank() || email.isBlank() || password.isBlank()) {
-            return Resource.Error("The fields can't be empty.")
-        }
-        return repository.register(username, email, password)
+class RegisterUseCase @Inject constructor(
+    private val repository: AuthRepository
+) {
+    suspend operator fun invoke(params: RegisterParams): Resource<Unit> {
+
+        val repositoryParams = RegisterParams(
+            firstName = params.firstName,
+            lastName = params.lastName,
+            email = params.email,
+            password = params.password,
+            role = params.role,
+            profileImageUri = params.profileImageUri,
+            companyName = params.companyName,
+            description = params.description,
+            photos = params.photos,
+            country = params.country,
+            city = params.city,
+            phoneNumber = params.phoneNumber,
+            address = params.address,
+            zipCode = params.zipCode
+        )
+
+        return repository.register(params = repositoryParams)
     }
 }
